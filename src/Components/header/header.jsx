4f51eae10080b07b335abda225ad7fd9aaed59f2
GabrielTnr <bielarandas88@gmail.com>
2023-11-29 14:26:45 -0300

    segundo-commit

---
+++ src/Components/header/header.jsx
@@ -0,0 +1,18 @@
+import "./header_style.scss";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>Início</li>
+            <li>Sobre</li>
+            <li>Projetos</li>
+            <li>Tecnologias</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
