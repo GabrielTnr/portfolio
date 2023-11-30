dc1d60e84cf89f474a3deb569780fc230f11f3d7
GabrielTnr <bielarandas88@gmail.com>
2023-11-30 19:28:13 -0300

    terceiro-commit

--- src/Components/inicio/inicio.jsx
+++ src/Components/inicio/inicio.jsx
@@ -1,17 +1,19 @@
-import "./header_style.scss";
+import "./inicio_style.scss";
 
-export default function Header() {
+export default function Inicio() {
   return (
     <>
       <header>
         <nav>
+          <h1>GABRIEL TENÓRIO</h1>
           <ul>
             <li>Início</li>
             <li>Sobre</li>
             <li>Projetos</li>
-            <li>Tecnologias</li>
+            <li>Contato</li>
           </ul>
         </nav>
+        <h1>Olá, eu sou um Desenvolvedor Front-End</h1>
       </header>
     </>
   )
---
+++ src/Components/sobre/sobre.jsx
@@ -0,0 +1,11 @@
+import "./inicio_style.scss"
+
+export default function Inicio() {
+  return (
+    <>
+      <main>
+        <h1>Lorem</h1>
+      </main>
+    </>
+  )
+}
