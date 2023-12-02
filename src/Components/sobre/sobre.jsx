2c03edf25ac89ddd740d3d385407f3a7f8b180d5
GabrielTnr <bielarandas88@gmail.com>
2023-12-02 12:05:07 -0300

    quarto-commit

--- src/Components/sobre/sobre.jsx
+++ src/Components/sobre/sobre.jsx
@@ -4,7 +4,36 @@ export default function Sobre() {
   return (
     <>
       <main>
-        <h1>Lorem</h1>
+        <h1>Estudos</h1> 
+        <section class="estudos">
+          <div>
+            <h1>2022-24</h1>
+            <p>Análise e Desenvolvimento de Sistemas - <span class="textoVerde">IBGM</span></p>
+          </div>
+          <div><h1>2023-23</h1>
+            <p>Curso de Desenvolvedor Front-End - <span class="textoVerde">VaiNaWeb</span></p>
+          </div>
+        </section>
+        <h1>Tecnologias</h1>
+        <section class="tecnologias">
+          <figure>
+            <div class="htmlIcon">
+            <img src="https://cdn-icons-png.flaticon.com/512/732/732212.png" alt="HTML Icon" />
+            </div>
+            <div class="cssIcon">
+            <img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt="CSS Icon" />
+            </div>
+            <div class="jsIcon">
+            <img src="https://i.imgur.com/VJF3Cia.png" alt="JavaScript Icon" />
+            </div>
+            <div class="reactIcon">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/free-react-3-1175109.png" alt="React Icon" />
+            </div>
+            <div class="sassIcon">
+            <img class="sassImg" src="https://i.imgur.com/uvNmPSG.png" alt="Sass Icon" />
+            </div>
+          </figure>
+        </section>
       </main>
     </>
   )
