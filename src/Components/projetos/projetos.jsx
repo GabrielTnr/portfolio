d22ecfd0b7fe5e326c09f5020f0e72e17047a418
GabrielTnr <bielarandas88@gmail.com>
2023-12-03 11:45:55 -0300

    quinto-commit

---
+++ src/Components/projetos/projetos.jsx
@@ -0,0 +1,21 @@
+import "./projetos_style.scss"
+
+export default function Projetos() {
+  return (
+    <>
+    <main>
+      <h1>Projetos</h1>
+      <figure class="projetos">
+        <img src="https://i.imgur.com/GNBnyZS.png" alt="" />
+        <img src="https://i.imgur.com/gzxmWBX.png" alt="" />
+        <img src="https://i.imgur.com/daldUvK.png" alt="" />
+      </figure>
+      <figure class="projetos">
+        <img src="https://i.imgur.com/x2GZ5fB.png" alt="" />
+        <img src="https://i.imgur.com/VihjT9i.png" alt="" />
+        <img src="https://i.imgur.com/RsshDzp.png" alt="" />
+      </figure>
+    </main>
+    </>
+  )
+}
